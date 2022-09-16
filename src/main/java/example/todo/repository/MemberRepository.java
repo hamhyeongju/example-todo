702043d715af2a9b733a4fd087175032f20f1968
hhj <gudwn357@gmail.com>
2022-09-16 23:05:23 +0900

    feat(LoginServiceImpl) : create LoginServiceImpl

--- src/main/java/example/todo/repository/MemberRepository.java
+++ src/main/java/example/todo/repository/MemberRepository.java
@@ -3,7 +3,9 @@ package example.todo.repository;
 import example.todo.Domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Member findByLoginId(String loginId);
+    Optional<Member> findByLoginId(String loginId);
 }
---
+++ src/main/java/example/todo/service/LoginService/LoginServiceImpl.java
@@ -0,0 +1,21 @@
+package example.todo.service.LoginService;
+
+import example.todo.Domain.Member;
+import example.todo.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class LoginServiceImpl implements LoginService{
+
+    private final MemberRepository repository;
+
+    @Override
+    public Optional<Member> login(String loginId, String password) {
+        Optional<Member> findMember = repository.findByLoginId(loginId);
+        return findMember.filter(member -> member.getPassword().equals(password));
+    }
+}
