d59563ad8f868a386a61f987029156e04946993c
hhj <gudwn357@gmail.com>
2022-09-16 23:03:25 +0900

    feat(LoginService) : create LoginService

---
+++ src/main/java/example/todo/service/LoginService/LoginService.java
@@ -0,0 +1,12 @@
+package example.todo.service.LoginService;
+
+import example.todo.Domain.Member;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public interface LoginService {
+
+    Optional<Member> login(String loginId, String password);
+}
