04b8e95765702850ade84802402cf41d4a20643e
hhj <gudwn357@gmail.com>
2022-09-17 16:58:25 +0900

    test : 회원 로그인 테스트 코드 작성

--- src/test/java/example/todo/repository/MemberRepositoryTest.java
+++ src/test/java/example/todo/repository/MemberRepositoryTest.java
@@ -1,13 +1,18 @@
 package example.todo.repository;
 
 import example.todo.Domain.Member;
+import example.todo.service.LoginService.LoginServiceImpl;
 import example.todo.service.memberService.MemberServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.useDefaultRepresentation;
 
 @SpringBootTest
 @Rollback(value = false)
@@ -15,6 +20,9 @@ class MemberRepositoryTest {
 
     @Autowired
     MemberServiceImpl service;
+
+    @Autowired
+    LoginServiceImpl loginService;
 
     @Test
     public void save() throws Exception {
@@ -24,11 +32,31 @@ class MemberRepositoryTest {
 
         //when
         Long savedId1 = service.save(member1);
-        Long savedId2 = service.save(member2);
+        Long savedId2 = service.save(member2); // loginId 같으므로 저장 실패
 
         //then
         assertThat(savedId1).isEqualTo(member1.getId());
         assertThat(savedId2).isNull();
+    }
+
+    @Test
+    public void login() throws Exception {
+        //given
+        Member member1 = new Member("123", "456", "member1");
+        service.save(member1);
+
+        //when
+        Optional<Member> loginMember1 = loginService.login("123", "456"); // O, O
+        Optional<Member> loginMember2 = loginService.login("123", "555"); // O, X
+        Optional<Member> loginMember3 = loginService.login("333", "123"); // X, O
+        Optional<Member> loginMember4 = loginService.login("333", "333"); // X, O
+
+        //then
+        assertThat(loginMember1.isPresent()).isTrue();
+        assertThat(loginMember2.isPresent()).isFalse();
+        assertThat(loginMember3.isPresent()).isFalse();
+        assertThat(loginMember4.isPresent()).isFalse();
+
     }
 
 }
