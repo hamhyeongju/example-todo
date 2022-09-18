32407f42f8e3f998fe9c901f42ed0319cb17da22
hhj <gudwn357@gmail.com>
2022-09-18 22:42:01 +0900

    feat(HomeController) : @GetMapping @PostMapping ('/add') >> >> 회원가입 로직, 컨트롤러 개발 이후 자세한 검증 로직 추가 예정

--- src/main/java/example/todo/controller/HomeController.java
+++ src/main/java/example/todo/controller/HomeController.java
@@ -1,9 +1,15 @@
 package example.todo.controller;
 
+import example.todo.Domain.Member;
+import example.todo.controller.dto.MemberDto;
 import example.todo.service.memberService.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,4 +27,17 @@ public class HomeController {
         return "/index";
     }
 
+    @GetMapping("/add")
+    public String addForm(@ModelAttribute MemberDto memberDto) {
+        return "/member/add";
+    }
+
+    @PostMapping("/add")
+    public String save(@Validated @ModelAttribute MemberDto memberDto, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) return "/member/add";
+
+        Member member = new Member(memberDto.getLoginId(), memberDto.getPassword(), memberDto.getName());
+        return memberService.save(member) != null ? "redirect:/" : "/member/add";
+    }
 }
