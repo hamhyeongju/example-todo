a667f6b2b586cc7fab3efcf24a7e8c4da1405da0
hhj <gudwn357@gmail.com>
2022-09-18 22:21:25 +0900

    feat(MemberDto) : create MemberDto

---
+++ src/main/java/example/todo/controller/dto/MemberDto.java
@@ -0,0 +1,16 @@
+package example.todo.controller.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
+@Getter @Setter
+public class MemberDto {
+    @NotNull
+    private String loginId;
+    @NotNull
+    private String password;
+    @NotNull
+    private String name;
+}
