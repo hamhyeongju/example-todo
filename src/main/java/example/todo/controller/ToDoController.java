a0656edce9e6d7407713aab6bf59cbc339ad7163
hhj <gudwn357@gmail.com>
2022-09-20 21:42:15 +0900

    feat : ToDoController CRUD 관련 메서드 생성

--- src/main/java/example/todo/controller/ToDoController.java
+++ src/main/java/example/todo/controller/ToDoController.java
@@ -3,14 +3,19 @@ package example.todo.controller;
 import example.todo.Domain.Member;
 import example.todo.Domain.ToDo;
 import example.todo.controller.dto.ToDoDto;
+import example.todo.service.memberService.MemberService;
 import example.todo.service.todoService.ToDoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component
@@ -18,6 +23,7 @@ import java.util.stream.Collectors;
 public class ToDoController {
 
     private final ToDoService toDoService;
+    private final MemberService memberService;
 
     @ModelAttribute("toDoDtos")
     public List<ToDoDto> toDoDtos(HttpServletRequest request) {
@@ -39,5 +45,53 @@ public class ToDoController {
                         new ToDoDto(toDo.getId(), toDo.getTitle(), toDo.getDescription(),
                                 toDo.getIsCompleted(), toDo.getCreatedDate(), toDo.getDueDate()))
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping("/todo")
+    public String main(@ModelAttribute("toDoDto") ToDoDto toDoDto) {
+        return "/todo/main";
+    }
+
+    @PostMapping("/todo/add")
+    public String addToDo(@Validated @ModelAttribute("toDoDto") ToDoDto toDoDto, BindingResult bindingResult, HttpServletRequest request) {
+
+        if (bindingResult.hasErrors()) {
+            return "/todo/main";
+        }
+
+        Optional<Member> findMember = memberService.findById(getSessionMember(request).getId());
+        Optional<ToDo> createToDo = findMember.map(member -> ToDo.createToDo(
+                toDoDto.getTitle(), toDoDto.getDescription(), toDoDto.getDueDate(),
+                member));
+        createToDo.ifPresent(toDo -> toDoService.save(toDo));
+
+        return "redirect:/main";
+    }
+
+    @PostMapping("/main/update")
+    public String update(@Validated @ModelAttribute("toDoDto") ToDoDto toDoDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/todo/main";
+        }
+
+        toDoService.update(toDoDto.getId(), toDoDto.getTitle(), toDoDto.getDescription(), toDoDto.getDueDate());
+        return "redirect:/main";
+    }
+
+    @PostMapping("/main/change")
+    public String change(@Validated @ModelAttribute("toDoDto") ToDoDto toDoDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/todo/main";
+        }
+
+        toDoService.update(toDoDto.getId(), toDoDto.getTitle(), toDoDto.getDescription(), toDoDto.getDueDate());
+        toDoService.changeStatus(toDoDto.getId());
+        return "redirect:/main";
+    }
+
+    @PostMapping("/main/delete")
+    public String delete(@ModelAttribute ToDoDto toDoDto) {
+        toDoService.findById(toDoDto.getId()).ifPresent(toDo -> toDoService.delete(toDo));
+        return "redirect:/main";
     }
 }
