d706b4cbdde247349fdc13d882bdae99ce3632ac
hhj <gudwn357@gmail.com>
2022-09-20 17:33:18 +0900

    feat(ToDoDto) : create ToDoDto

---
+++ src/main/java/example/todo/controller/dto/ToDoDto.java
@@ -0,0 +1,31 @@
+package example.todo.controller.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Getter @Setter
+public class ToDoDto {
+
+    private Long id;
+    @NotNull
+    private String title;
+    private String description;
+    private Boolean isCompleted = false;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate createdDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dueDate;
+
+    public ToDoDto(Long id, String title, String description, Boolean isCompleted, LocalDate createdDate, LocalDate dueDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.isCompleted = isCompleted;
+        this.createdDate = createdDate;
+        this.dueDate = dueDate;
+    }
+}
