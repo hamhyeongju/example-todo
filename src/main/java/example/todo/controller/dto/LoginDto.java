eb40da70211737e8402f8c686bee955fbaf49132
hhj <gudwn357@gmail.com>
2022-09-25 20:08:57 +0900

    refactor(LoginDto) : @NotEmpty 추가
    NotEmpty를 사전에 검증하여 미입력시 쿼리가 발생하지 않도록 함

--- src/main/java/example/todo/controller/dto/LoginDto.java
+++ src/main/java/example/todo/controller/dto/LoginDto.java
@@ -4,10 +4,11 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Getter @Setter
 public class LoginDto {
+    @NotEmpty
     private String loginId;
+    @NotEmpty
     private String password;
 }
