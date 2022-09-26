c988532f2fb8b04e9c96cf3fa7ca6f33e41703c7
hhj <gudwn357@gmail.com>
2022-09-26 16:56:11 +0900

    feat(Configurer) : create Configurer
    LoginInterceptor 등록

---
+++ src/main/java/example/todo/configuration/Configurer.java
@@ -0,0 +1,18 @@
+package example.todo.configuration;
+
+import example.todo.configuration.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Configurer implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/login", "/add", "/error");
+    }
+}
