4ac9f1feffb47c099b1d3067e5f818479e128231
hhj <gudwn357@gmail.com>
2022-11-25 16:03:55 +0900

    feat : add SecurityFileterChain

--- src/main/java/example/todo/configuration/Configurer.java
+++ src/main/java/example/todo/configuration/Configurer.java
@@ -4,7 +4,10 @@ import example.todo.configuration.interceptor.LoginInterceptor;
 import example.todo.configuration.interceptor.ToDoInterceptor;
 import example.todo.service.todoService.ToDoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,6 +16,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class Configurer implements WebMvcConfigurer {
 
     private final ToDoService toDoService;
+
+    /**
+     * spring security
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable().authorizeRequests()
+                .antMatchers("/", "/login", "/add", "/error", "/css/**", "/js/**").permitAll()
+                .antMatchers("/**").authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/todo")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/");
+
+        return http.build();
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
