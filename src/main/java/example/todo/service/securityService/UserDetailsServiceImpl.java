f30bc4339ce4471da3838dd9a4a4595ef2116fea
hhj <gudwn357@gmail.com>
2022-11-25 17:47:24 +0900

    feat : create UserDetailsServiceImpl

---
+++ src/main/java/example/todo/service/securityService/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package example.todo.service.securityService;
+
+
+import example.todo.Domain.Member;
+import example.todo.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final MemberRepository repository;
+
+    // id로 1차 조회 (2차는 provider에서 암호화된 비밀번호)
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new CustomUserDetails(repository.findByLoginId(username).orElseThrow(() ->
+                new UsernameNotFoundException("해당 사용자가 존재하지 않습니다. : " + username)));
+    }
+
+}
