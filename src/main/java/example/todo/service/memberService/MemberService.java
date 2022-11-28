ade4282151da58049e5e2cfb9b2ca198e93f7a98
hhj <gudwn357@gmail.com>
2022-11-28 15:57:37 +0900

    feat (MemberService) : create method saveBySecurity

--- src/main/java/example/todo/service/memberService/MemberService.java
+++ src/main/java/example/todo/service/memberService/MemberService.java
@@ -1,6 +1,7 @@
 package example.todo.service.memberService;
 
 import example.todo.Domain.Member;
+import example.todo.controller.dto.MemberDto;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,6 +9,8 @@ import java.util.Optional;
 public interface MemberService {
 
     Long save(Member member);
+
+    Long saveBySecurity(MemberDto memberDto);
 
     List<Member> findAll();
 
--- src/main/java/example/todo/service/memberService/MemberServiceImpl.java
+++ src/main/java/example/todo/service/memberService/MemberServiceImpl.java
@@ -1,8 +1,10 @@
 package example.todo.service.memberService;
 
 import example.todo.Domain.Member;
+import example.todo.controller.dto.MemberDto;
 import example.todo.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,15 +14,27 @@ import java.util.Optional;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class MemberServiceImpl implements MemberService{
+public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository repository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     @Transactional
     public Long save(Member member) {
         // loginId 중복 체크
         return repository.findByLoginId(member.getLoginId()).isEmpty() ? repository.save(member).getId() : null;
+    }
+
+    @Override
+    @Transactional
+    public Long saveBySecurity(MemberDto memberDto) {
+        return repository.findByLoginId
+                (memberDto.getLoginId()).isEmpty() ? repository.save(
+                new Member(
+                        memberDto.getLoginId(),
+                        bCryptPasswordEncoder.encode(memberDto.getPassword()),
+                        memberDto.getName().strip())).getId() : null;
     }
 
     @Override
