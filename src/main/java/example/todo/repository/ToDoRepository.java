82aa74cb3c046752a52eda35fe1f90086f614ddd
hhj <gudwn357@gmail.com>
2022-12-02 17:41:46 +0900

    feat (ToDoRepository) : create JPQL query
    spring data jpa를 사용하면 필요없는 join이 발생하여 query 성능 향상을 위해 JPQL로 바꿈

--- src/main/java/example/todo/repository/ToDoRepository.java
+++ src/main/java/example/todo/repository/ToDoRepository.java
@@ -3,6 +3,8 @@ package example.todo.repository;
 import example.todo.Domain.ToDo;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,4 +13,7 @@ public interface ToDoRepository extends JpaRepository<ToDo, Long> {
     List<ToDo> findToDoListByMemberIdAndIsCompleted(Long memberId, Boolean isCompleted);
 
     List<ToDo> findSortByMemberIdAndIsCompleted(Long memberId, Boolean isCompleted, Sort sort);
+
+    @Query("select t from ToDo t where t.member.id = :memberId and t.isCompleted = :isCompleted order by t.dueDate nulls last, t.createdDateTime nulls last")
+    List<ToDo> findJPQLByMemberIdAnAndIsCompleted(@Param("memberId") Long memberId, @Param("isCompleted") Boolean isCompleted);
 }
